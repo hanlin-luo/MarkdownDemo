46cadee426ea8f117b2bfdba5c5f9a0514e34c40
hanlin <hanlin.luo2@gmail.com>
2025-12-31 01:53:45 -0600

    Initial Commit

---
+++ MarkdownDemo/MarkdownDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownDemoApp.swift
+//  MarkdownDemo
+//
+//  Created by lmc on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
